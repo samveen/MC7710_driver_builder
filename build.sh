b0a4b9f22004fd30c580ce73ce775acb3fb7513d
Samveen <samveen@yahoo.com>
2019-07-03 13:14:12 +0530

    Cleanup section improved and moved to end

--- build.sh
+++ build.sh
@@ -32,12 +32,16 @@ sed -i 's/^\(#define MEMCG_NOT_FIX\)$/\/* \1 *\//' GobiNet/GobiUSBNet.c
 make -C GobiNet
 make -C GobiSerial
 
-# undo
-for f in $FILELIST; do
-    sudo mv ${f}.bak ${f}
-done
-
 # install
 sudo install Gobi*/*.ko -t /lib/modules/$(uname -r)/updates/dkms/
 
+# Rebuild module dependencies
 sudo depmod
+
+# undo all changes
+for f in $FILELIST; do
+    sudo mv ${f}.bak ${f}
+done
+sed -i 's!^/\* \(#define MEMCG_NOT_FIX\) \*/$!\1!' GobiNet/GobiUSBNet.c
+make -C GobiNet clean
+make -C GobiSerial clean
